75afa93a3ea274a8f5a29489ac28bcf5e4952108
Hisyam <mhisyama2007@gmail.com>
2025-11-13 23:13:25 +0700

    fix env for render production and css asset

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ranking MHS - Admin</title>
-    <link rel="stylesheet" href="css/dashboard.css">
+    <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
 </head>
 <body>
     <div class="navbar">
